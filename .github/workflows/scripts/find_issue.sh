cd6c59a95acfbad5ea6dec7781e2dda5cd63c9e8
Janis Wehen <janiswehen@gmail.com>
2023-10-01 00:44:30 +0200

    asdasd

--- .github/workflows/scripts/find_issue.sh
+++ .github/workflows/scripts/find_issue.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# Assumes GITHUB_TOKEN and GITHUB_REPOSITORY are environment variables
-
-branch_name="$1" # Extract branch name from GITHUB_REF
+branch_name="$1" # The first argument is the branch name
 
 # Extract the longest digit prefix of branch_name
 digit_prefix=$(echo "$branch_name" | grep -o '^[0-9]*')
@@ -11,9 +9,9 @@ digit_prefix=$(echo "$branch_name" | grep -o '^[0-9]*')
 length=${#digit_prefix}
 
 # Exit if number is not of length 4 or 5
-if [[ $length -eq 0 ]] || [[ $length -eq 1 ]]; then
-    echo "The digit prefix length is neither 0 nor 1. Exiting."
-    exit 0
+if [[ $length -ne 4 ]] && [[ $length -ne 5 ]]; then
+    echo "The digit prefix length is neither 4 nor 5. Exiting."
+    exit 1
 fi
 
 # Fetch the issue details using GitHub API
@@ -21,11 +19,31 @@ issue_data=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
                   -H "Accept: application/vnd.github.v3+json" \
                   "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${digit_prefix}")
 
-# Extract issue title from the response using jq
+# Check if issue exists
 issue_title=$(echo "$issue_data" | jq -r '.title')
 
-if [ "$issue_title" != "null" ]; then
-    echo "Issue found with title: $issue_title"
-else
+if [ "$issue_title" == "null" ]; then
     echo "No issue found with number: $digit_prefix"
+    exit 0
+else
+    echo "Issue found with title: $issue_title"
+fi
+
+# Extract issue labels with jq
+labels_json=$(echo "$issue_data" | jq '.labels[] .name')
+
+# Apply the labels to the pull request
+pr_number=$(echo "${GITHUB_REF##*/}" | grep -oE '[0-9]+')
+response=$(curl -s -X PUT \
+               -H "Authorization: token ${GITHUB_TOKEN}" \
+               -H "Accept: application/vnd.github.v3+json" \
+               -d "{\"labels\": $labels_json}" \
+               "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${pr_number}")
+
+# Check if labels were applied successfully using jq
+if [[ $(echo "$response" | jq '.labels') ]]; then
+    echo "Labels applied to PR successfully."
+else
+    echo "Failed to apply labels to PR."
+    exit 1
 fi
