3e5fb7bbde40f29c1f484fbf872b2ecc5efde512
Janis Wehen <janiswehen@gmail.com>
2023-10-01 01:46:05 +0200

    sdf

--- .github/workflows/scripts/find_issue.sh
+++ .github/workflows/scripts/find_issue.sh
@@ -39,8 +39,8 @@ response=$(curl -L \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER}/labels \
-    -d '{"labels":["bug", "invalid"]}' \
+    https://api.github.com/repos/OWNER/REPO/issues/${PR_NUMBER}/labels \
+    -d '{"labels":["bug","enhancement"]}' \
     -w "\nHTTP_STATUS:%{http_code}\n" 2>&1)
 
 echo "Response from GitHub API:"
