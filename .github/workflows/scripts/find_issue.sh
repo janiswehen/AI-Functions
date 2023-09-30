b6ab1f5f90f58048e8a72719ab7d88a90e6743ab
Janis Wehen <janiswehen@gmail.com>
2023-10-01 01:35:57 +0200

    asd

--- .github/workflows/scripts/find_issue.sh
+++ .github/workflows/scripts/find_issue.sh
@@ -31,14 +31,17 @@ fi
 labels_json=$(echo "$issue_data" | jq '.labels[] .name' | tr '\n' ',' | sed 's/,$//' | awk '{print "["$0"]"}')
 echo "Extracted labels: $labels_json"
 
+echo $GITHUB_REPOSITORY
+
 # Apply the labels to the pull request
-response=$(curl -s -X POST \
-               -H "Accept: application/vnd.github.v3+json" \
-               -H "Authorization: token ${GITHUB_TOKEN}" \
-               -H "X-GitHub-Api-Version: 2022-11-28" \
-               "https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER}/labels" \
-               -d "{\"labels\":${labels_json}}" \
-               -w "\nHTTP_STATUS:%{http_code}\n" 2>&1)
+response=$(curl -L \
+    -X PUT \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER}/labels \
+    -d '{"labels":["bug", "invalid"]}' \
+    -w "\nHTTP_STATUS:%{http_code}\n" 2>&1)
 
 echo "Response from GitHub API:"
 echo "$response"
