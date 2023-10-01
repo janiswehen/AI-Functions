dec05058c34697b26040fa8ea4d9221656dcfa2c
Janis Wehen <janiswehen@gmail.com>
2023-10-01 17:05:16 +0200

    a,.n,mnasd,mn

--- .github/workflows/scripts/release_label.sh
+++ .github/workflows/scripts/release_label.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "test"
+source .github/workflows/scripts/github_utils.sh
+
+# Define the release label
+release_label_json='Release'
+
+# Apply the release label to the PR
+apply_labels_to_issue_or_pr "$GITHUB_TOKEN" "$GITHUB_REPOSITORY" "$PR_NUMBER" "[\"$release_label_json\"]" || exit 0
+
+# Extract the issue number based on the branch name
+issue_number=$(get_issue_number "$BRANCH_NAME") || exit 0
+
+# If you also want to apply the label to the associated issue:
+apply_labels_to_issue_or_pr "$GITHUB_TOKEN" "$GITHUB_REPOSITORY" "$issue_number" "$release_label_json" || exit 0
