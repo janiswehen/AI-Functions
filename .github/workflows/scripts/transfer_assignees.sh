ba11320ebede66e4e33adb6efa9839133f875407
Janis Wehen <janiswehen@gmail.com>
2023-10-02 23:33:38 +0200

    asdasdasd

--- .github/workflows/scripts/transfer_assignees.sh
+++ .github/workflows/scripts/transfer_assignees.sh
@@ -8,7 +8,5 @@ issue_number=$(get_issue_number "$BRANCH_NAME") || exit 0
 # Get assignees associated with the issue number
 assignees_json=$(get_assignees_to_issue_or_pr "$GITHUB_TOKEN" "$GITHUB_REPOSITORY" "$issue_number") || exit 0
 
-echo "$assignees_json"
-
 # Apply those assignees to the PR
 apply_assignees_to_issue_or_pr "$GITHUB_TOKEN" "$GITHUB_REPOSITORY" "$PR_NUMBER" "$assignees_json" || exit 0
